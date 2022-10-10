e92767fc88e4b363390da324f3d85aa92bd120c2
qnkong01 <qnkong01@mail.yst.com.cn>
2022-10-10 15:28:55 +0800

    fix(吸顶失效): 外层加宽高及滚动

--- examples/vue2/src/App.vue
+++ examples/vue2/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <van-sticky>
-      <div style="height:100px; background: #ccc">吸顶元素</div>
-    </van-sticky>
-    <div style="height:500px; background: #ebeef5">长元素</div>
-    <div id="nav">
-      <router-link to="/home">首页</router-link> | <router-link to="/dialog">弹窗</router-link> |
-      <router-link to="/location">路由</router-link> | <router-link to="/communication">通信</router-link>
+    <div style="position: relative; width: 100vw; height: 100vh; overflow-y: scroll">
+      <van-sticky>
+        <div style="height: 100px; background: #ccc">吸顶元素</div>
+      </van-sticky>
+      <div style="height: 500px; background: #ebeef5">长元素</div>
+      <div id="nav">
+        <router-link to="/home">首页</router-link> | <router-link to="/dialog">弹窗</router-link> |
+        <router-link to="/location">路由</router-link> | <router-link to="/communication">通信</router-link>
+      </div>
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
 <script>
-import VanSticky from "./components/VanSticky"
+// import VanSticky from "./components/VanSticky";
 export default {
   watch: {
     // 在 vue2-sub 路由下主动告知主应用路由跳转，主应用也跳到相应路由高亮菜单栏
@@ -22,7 +24,7 @@ export default {
     },
   },
   components: {
-    VanSticky,
+    // VanSticky,
   },
 };
 </script>
